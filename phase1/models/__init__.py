55a726e26f59628afb52e204036fef4cd3f9f357
sabry_youssef <vendorah2@gmail.com>
2025-05-31 14:10:37 +0300

    Update __init__.py

--- phase1/models/__init__.py
+++ phase1/models/__init__.py
@@ -1,16 +1,18 @@
-from . import exception
-from . import move
-from . import rating
-from . import document_request
-from . import partner
-from . import product
-from . import document
-from . import project_fields
-from . import task_document
-from . import project
-from . import sale
-from . import task
-from . import sov
-from . import documents
+from . import sale,product,project,task,partner,move,project_fields
+from . import sov,rating,document,documents,exception, document_request, models
+## from . import exception
+## from . import move
+## from . import rating
+## from . import document_request
+## from . import partner
+##from . import product
+## from . import document
+## from . import project_fields
+## from . import task_document
+## from . import project
+## from . import sale
+## from . import task
+## from . import sov
+## from . import documents
 from . import partner_shareholder
-from . import models
+## from . import models
